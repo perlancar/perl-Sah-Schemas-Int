e8c4b77d2c6fd7c47b9a7f17e0ec7c35ea79f296
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-08 18:57:04 +0700

    Fix schemas

--- lib/Sah/Schema/Int.pm
+++ lib/Sah/Schema/Int.pm
@@ -1,11 +1,11 @@
 package Sah::Schema::Int;
 
+# DATE
+# VERSION
+
 use 5.010001;
 use strict;
 use warnings;
-
-# VERSION
-# DATE
 
 our %SCHEMAS = (
     pos_int => [int => {
@@ -38,55 +38,55 @@ _
         min     => -2**7,
         max     => +2**7-1,
     }],
-    uint8 => [
+    uint8 => [int => {
         summary => '8-bit unsigned integer',
         min     => +0,
         max     => +2**8-1,
-    ],
+    }],
 
     int16 => [int => {
         summary => '16-bit signed integer',
         min     => -2**15,
         max     => +2**15-1,
     }],
-    uint16 => [
+    uint16 => [int => {
         summary => '16-bit unsigned integer',
         min     => +0,
         max     => +2**16-1,
-    ],
+    }],
 
     int32 => [int => {
         summary => '32-bit signed integer',
         min     => -2**31,
         max     => +2**31-1,
     }],
-    uint32 => [
+    uint32 => [int => {
         summary => '32-bit unsigned integer',
         min     => 0,
         max     => 2**32-1,
-    ],
+    }],
 
     int64 => [int => {
         summary => '64-bit signed integer',
         min     => -2**63,
         max     => +2**63-1,
     }],
-    uint64 => [
+    uint64 => [int => {
         summary => '64-bit unsigned integer',
         min     => 0,
         max     => 2**64-1,
-    ],
+    }],
 
     int128 => [int => {
         summary => '32-bit signed integer',
         min     => -2**127,
         max     => +2**127-1,
     }],
-    uint128 => [
+    uint128 => [int => {
         summary => '128-bit unsigned integer',
         min     => 0,
         max     => 2**128-1,
-    ],
+    }],
 );
 
 1;
