832f4621dfb1e524ebb4b5b5b4fa7a6eea6c04ce
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-07-16 17:58:50 +0700

    [doc] Mention Sah::Schemas::IntRange, Sah::PSchemas::IntRange

--- lib/Sah/Schemas/Int.pm
+++ lib/Sah/Schemas/Int.pm
@@ -1,7 +1,15 @@
 package Sah::Schemas::Int;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 1;
 # ABSTRACT: Sah schemas for various integers
+
+=head1 prepend:SEE ALSO
+
+L<Sah::Schemas::IntRange>
+
+L<Sah::PSchemas::IntRange>
